20629b16d1a3ec4abed717f7d7ae97f38c4cad3d
ragranados <raul.granados12@gmail.com>
2021-08-22 21:42:09 -0600

    Adding TODO

--- src/middlewares/grantRoles.js
+++ src/middlewares/grantRoles.js
@@ -4,14 +4,16 @@ const roles = require('../constants/roles');
  * Middleware
  */
 
+//TODO: Add or remove roles as needed
+
 exports.grantRoles = (req, res, next) => {
     const grantedRoles = [];
 
-    if(req.user.role === roles.USER){
+    if (req.user.role === roles.USER) {
         grantedRoles.push(roles.USER);
     }
 
-    if(req.user.role === roles.ADMIN){
+    if (req.user.role === roles.ADMIN) {
         grantedRoles.push(roles.USER);
         grantedRoles.push(roles.ADMIN);
     }
